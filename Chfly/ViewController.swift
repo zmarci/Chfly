b4e69db56cd4cd569206488672cf41380c21a5c3
zmarci <z.marton13@gmail.com>
2017-07-03 08:34:36 +0200

    Beginning

---
+++ Chfly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chfly
+//
+//  Created by Marton Zatrok on 03/07/17.
+//  Copyright © 2017 Marton Zatrok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Chfly' do
+ 
+pod 'SwiftyJSON'
+
+end
